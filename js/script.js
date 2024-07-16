a776fd7ebfb06609ae6010e65dd19d68751681f4
samilessons <samisecblog@gmail.com>
2024-07-16 22:48:57 +0400

    we added status for users which film count will we more than 0 and eddited answers and add for loop

--- js/script.js
+++ js/script.js
@@ -7,24 +7,31 @@ const DB = {
   movies: {},
   actors: {},
   genres: [],
-  private: false
+  status: null,
+  private: false,
 };
 
-const a = prompt("Ո՞ր ֆիլմն եք վերջերդ դիտել");
-const b = +prompt("Ինչքա՞ն եք գնահատում այդ ֆիլմը");
-const c = prompt("Ո՞ր ֆիլմն եք վերջերդ դիտել");
-const d = +prompt("Ինչքա՞ն եք գնահատում այդ ֆիլմը");
+for (let i = 0; i < 2; i++) {
+  const filmName = prompt("Ո՞ր ֆիլմն եք վերջերդ դիտել");
+  const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
+  
+  if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
+    DB.movies[filmName] = filmRate;
+    console.log("Resolve");
+  } else {
+    console.log("Reject");
+    i--;
+  }
+}
 
-DB.movies[a] = b;
-DB.movies[c] = d;
+if (DB.count < 10) {
+  DB.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
+} else if (DB.count >= 10 && DB.count < 30) {
+  DB.status = "Դուք դասական ֆիլմ դիտող եք";
+} else if (DB.count >= 30) {
+  DB.status = "Դուք կինոման եք !";
+} else {
+  console.log("Տեղի է ունեցել ինչ-որ խնդիր");
+}
 
 console.log(DB);
-
-/* 
-  1․ ստանալ ֆիլմի անունը և գնահատականը
-  2․ ոնց ենք ավելացնելու օբյեկտի մեջ
-  movies: {
-    "titanik": 10,
-    "bad boys": 7.5
-  }
-*/
